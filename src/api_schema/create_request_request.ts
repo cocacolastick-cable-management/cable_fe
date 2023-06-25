0d2b07c3a92c07ae66e8d40698558e44eab07cde
Say my name <vuphamlethanh.public@gmail.com>
2023-06-25 13:45:16 +0700

    feat<api_shema>: create `CreateRequestRequest`

---
+++ src/api_schema/create_request_request.ts
@@ -0,0 +1,12 @@
+interface CreateRequestRequest
+{
+   cableAmount: number
+   contractCounter: number
+   contractorEmail: string
+}
+
+// "cableAmount": 110,
+//    "contractCounter": 2,
+//    "contractorEmail": "contractor@yopmail.com"
+
+export default CreateRequestRequest
